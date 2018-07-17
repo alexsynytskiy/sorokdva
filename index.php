64c7b47223b3c7ff8e89fe9e4eaf79a20a629ac4
synytskyi.oleksa <synytskyi.oleksa@pdffiller.team>
2018-07-17 18:02:45 +0300

    Init project

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:og="http://ogp.me/ns#"
+      xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=0.5, user-scalable=no" />
+
+    <title>Агентство 42</title>
+
+    <meta property="og:url" content="http://sorokdva.com.ua" />
+    <meta property="og:image" content="http://sorokdva.com.ua/images/fb_share.jpg" />
+    <meta property="og:title" content="Агентство 42" />
+    <meta property="og:description" content="42 - наверное, лучшее ивент агентство" />
+    <meta property="og:type" content="website" />
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/images/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/images/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/images/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/images/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/images/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/images/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/images/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/images/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/images/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/images/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class='logo box-scene'>
+                <div class='box'>
+                    <div class='front face'>
+                        <a href="http://sorokdva.com.ua/"><img src="/images/logo-black.png" width="140" height="160" alt="Агентство 42" border="0" /></a>
+                    </div>
+                    <div class="side face">
+                        <a href="http://sorokdva.com.ua/"><img src="/images/logo-green.png" width="140" height="160" alt="Агентство 42" border="0" /></a>
+                    </div>
+                </div>
+            </div>
+            <div class="download">
+                <a href="/42-Agency-Showcase.pdf" target="_blank"><img src="/images/download-icon.png" width="140" height="50" alt="Скачать презентацию" /></a>
+            </div>
+            <div class="blog">
+                <a href="/blog/" target="_blank"><img src="/images/blog-icon.png" width="140" height="50" alt="Читать наш блог" /></a>
+            </div>
+            <div class="eventlab">
+                <a href="http://www.42eventlab.com.ua/" target="_blank"><img src="/images/event_lab.png" width="140" height="50" alt="Event Lab 42" /></a>
+            </div>
+        </div>
+
+        <div class="logo-main">
+            <img src="/images/1.png">
+            <div class="mission">
+                Місія 42
+            </div>
+            <div class="title-text">
+                Допомагати людям і компаніям втілювати їхні мрії інструментами івентів
+            </div>
+
+            <div class="container-fluid info-text" id="mobile-info-text">
+                  <div class="row">
+                    <div class="col-md-12">
+                        <div class="col-md-6 col-xs-6">
+                            <div class="text-block-info">
+                                <div class="head clearfix">
+                                    <div class="symbol"></div>
+                                    <div class="title">Щирість та порядність</div>
+                                </div>
+                                <div class="text">
+                                    Ми не використовуємо чужих ідей, відмовляємося від сумнівних тендерів та будуємо наші стосунки з Партнерами на принципах взаємозалежності, взаємоповаги: рівний - рівному
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xs-6">
+                            <div class="text-block-info">
+                                <div class="head clearfix">
+                                    <div class="symbol"></div>
+                                    <div class="title">Пошук <br>рішень</div>
+                                </div>
+                                <div class="text">
+                                    Для нас індивідуальний підхід до Клієнта не просто слова. Це головна заповідь для створення нового продукту: під кожне завдання - своє рішення, на кожен виклик - ідея
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xs-6">
+                            <div class="text-block-info">
+                                <div class="head clearfix">
+                                    <div class="symbol"></div>
+                                    <div class="title">Якість <br>як принцип</div>
+                                </div>
+                                <div class="text">
+                                    Ми пишаємося якістю наших проектів. В кожен проект ми вкладаємо максимум своєї відповідальності і натхнення
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xs-6">
+                            <div class="text-block-info">
+                                <div class="head clearfix">
+                                    <div class="symbol"></div>
+                                    <div class="title">Повага <br>до праці</div>
+                                </div>
+                                <div class="text">
+                                    Ми не зриваємо терміни, поважаємо свій час, свою працю, час та працю наших партнерів, працюємо із прозорими кошторисами та виставляємо адекватну комісію
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xs-6">
+                            <div class="text-block-info">
+                                <div class="head clearfix">
+                                    <div class="symbol"></div>
+                                    <div class="title">Бути <br>на хвилі</div>
+                                </div>
+                                <div class="text">
+                                    Втілювати нове, відстоювати інновації, бути допитливим, впроваджувати ідеї з інших сфер
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xs-6">
+                            <div class="text-block-info">
+                                <div class="head clearfix">
+                                    <div class="symbol"></div>
+                                    <div class="title">Розвиток</div>
+                                </div>
+                                <div class="text">
+                                    Ми створюємо максимально можливі умови для розвитку співробітників, намагаємося розвиватися командою, допомагати розвиватися Партнерам і Клієнтам
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 col-xs-12">
+                            <div class="text-block-info" style="display: block; margin: 0 auto;">
+                                <div class="head clearfix">
+                                    <div class="symbol"></div>
+                                    <div class="title">Команда і атмосфера</div>
+                                </div>
+                                <div class="text">
+                                    У нас класно працювати, інколи навіть не хочеться йти додому;) наші колеги не просто звичайні люди - вони велика мотивуюча команда однодумців
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid info-text" id="desktop-info-text">
+          <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-4 col-xs-6">
+                    <div class="text-block-info">
+                        <div class="head clearfix">
+                            <div class="symbol"></div>
+                            <div class="title">Щирість та порядність</div>
+                        </div>
+                        <div class="text">
+                            Ми не використовуємо чужих ідей, відмовляємося від сумнівних тендерів та будуємо наші стосунки з Партнерами на принципах взаємозалежності, взаємоповаги: рівний - рівному
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-6">
+                    <div class="text-block-info"></div>
+                </div>
+                <div class="col-md-4 col-xs-6">
+                    <div class="text-block-info" style="float: right;">
+                        <div class="head clearfix">
+                            <div class="title" style="text-align: left;">Пошук <br>рішень</div>
+                            <div class="symbol"></div>
+                        </div>
+                        <div class="text">
+                            Для нас індивідуальний підхід до Клієнта не просто слова. Це головна заповідь для створення нового продукту: під кожне завдання - своє рішення, на кожен виклик - ідея
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-6">
+                    <div class="text-block-info">
+                        <div class="head clearfix">
+                            <div class="symbol"></div>
+                            <div class="title">Команда і атмосфера</div>
+                        </div>
+                        <div class="text">
+                            У нас класно працювати, інколи навіть не хочеться йти додому;) наші колеги не просто звичайні люди - вони велика мотивуюча команда однодумців
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-6">
+                    <div class="text-block-info"></div>
+                </div>
+                <div class="col-md-4 col-xs-6">
+                    <div class="text-block-info" style="float: right;">
+                        <div class="head clearfix">
+                            <div class="title" style="text-align: left;">Повага <br>до праці</div>
+                            <div class="symbol"></div>
+                        </div>
+                        <div class="text">
+                            Ми не зриваємо терміни, поважаємо свій час, свою працю, час та працю наших партнерів, працюємо із прозорими кошторисами та виставляємо адекватну комісію
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-6">
+                    <div class="text-block-info">
+                        <div class="head clearfix">
+                            <div class="symbol"></div>
+                            <div class="title">Бути <br>на хвилі</div>
+                        </div>
+                        <div class="text">
+                            Втілювати нове, відстоювати інновації, бути допитливим, впроваджувати ідеї з інших сфер
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-6">
+                    <div class="text-block-info" style="display: block; margin: 0 auto;">
+                        <div class="head clearfix">
+                            <div class="title" style="text-align: left; line-height: 58px;">Розвиток</div>
+                            <div class="symbol"></div>
+                        </div>
+                        <div class="text">
+                            Ми створюємо максимально можливі умови для розвитку співробітників, намагаємося розвиватися командою, допомагати розвиватися Партнерам і Клієнтам
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-6">
+                    <div class="text-block-info" style="float: right;">
+                        <div class="head clearfix">
+                            <div class="title" style="text-align: left;">Якість <br>як принцип</div>
+                            <div class="symbol"></div>
+                        </div>
+                        <div class="text">
+                            Ми пишаємося якістю наших проектів. В кожен проект ми вкладаємо максимум своєї відповідальності і натхнення
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- <div class="container">
+            <iframe class="video" src="//youtube.com/embed/3hYi2-j5kCg?playlist=3hYi2-j5kCg&autoplay=1&badge=0&title=0&portrait=0&autoplay=1&autohide=1&cc_load_policy=0&controls=2&iv_load_policy=3&loop=1&showinfo=0" frameborder="0" allowfullscreen></iframe>
+        </div> -->
+
+        <div class="address" style="display: none;">
+            <p>
+                <span><a target="_blank" href="https://goo.gl/maps/m7XhuskgbEv">Киев, ул. Якира 8, офис 17</a>, +380 (67) 401-84-49,</span> 
+                <span><a href="mailto:info@sorokdva.com.ua">info@sorokdva.com.ua</a></span>
+                <a href="https://www.facebook.com/sorokdva" target="_blank" class="fb">
+                    <img src="/images/facebook-icon.png" width="20" height="20" />
+                </a>
+            </p>
+        </div>
+    </div>
+    <link rel="stylesheet" href="/sorokdva.css?v=7"/>
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <script type="text/javascript" src="/sorokdva.js?v=2"></script>
+    <script>
+        (function (i, s, o, g, r, a, m) {i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () { (i[r].q = i[r].q || []).push(arguments)}, i[r].l = 1 * new Date(); a = s.createElement(o), m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-68182017-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+</body>
+</html>
